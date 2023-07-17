9b8c4b0b7ba654982fc998545371cc69d1fa808f
OscarZhong96 <138770114+OscarZhong96@users.noreply.github.com>
2023-07-17 17:40:01 +0100

    Update online_recipe_management_system.py
    Signed-off-by: OscarZhong96 <138770114+OscarZhong96@users.noreply.github.com>

--- online_recipe_management_system.py
+++ online_recipe_management_system.py
@@ -1,1 +1,34 @@
 class recipe:
+  def__init__(self,title,ingredients,instructions,cooking_time,dietary_info)
+  self.title= title
+  self.ingredients= ingredients
+  self.instructions= instructions
+  self.cooking_time= cooking_time
+  self.dietary_info= dietary_info
+macaroniandcheese= recipe("Macaroni and cheese",
+"\n 50g baguette cut into small chunks
+\n2 tbsp butter, plus 1 tbsp melted
+\n350g spiral or other short pasta
+\n1 garlic clove, finely chopped
+\n1 tsp English mustard powder
+\n3 tbsp plain flour
+\n500ml whole milk
+\n250g vegetarian mature cheddar, grated
+\n50g parmesan (or vegetarian alternative), grated",
+"\nPrep 10 min
+\n Cook 40 min",
+Nutrition: per serving
+\nkcal 860
+\nfat 42g
+\nsaturates 25g
+\ncarbs 88g
+\nsugars 9g
+\nfibre 0g
+\nprotein 38g
+\nsalt 1.9g
+
+class recipe_manager:
+  def__init__(self)
+  self.add_recipe
+  self.update_recipe
+  self.delete_recipe
